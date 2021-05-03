d11ae4657119ccfc21fcca7f52a85cbbfaa22f34
yuri.pereira <yuri.pereira@ambevtech.com.br>
2021-05-03 11:28:44 -0300

    aula 286 Custumizando o drawer

--- lib/components/custom_drawer/custom_drawer.dart
+++ lib/components/custom_drawer/custom_drawer.dart
@@ -1,12 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:xlo_mobx/components/custom_drawer/page_section.dart';
+
+import 'custom_drawer_header.dart';
 
 class CustomDrawer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      width: MediaQuery.of(context).size.width * 0.7,
-      child: Drawer(
-
+    ///ClipRRect é facil para recortar widgets...
+    return ClipRRect(
+      borderRadius: BorderRadius.horizontal(right: Radius.circular(50)),
+      child: SizedBox(
+        width: MediaQuery.of(context).size.width * 0.6,
+        child: Drawer(
+          child: ListView(
+            children: [
+              CustomDrawerHeader(),
+              PageSection()
+            ],
+          ),
+        ),
       ),
     );
   }
---
+++ lib/components/custom_drawer/page_tile.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class PageTile extends StatelessWidget {
+
+  PageTile({this.label, this.iconData, this.onTap, this.highlighted});
+  final String label;
+  final IconData iconData;
+  final VoidCallback onTap;
+  final bool highlighted;
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      title: Text(label, style: TextStyle(color: highlighted ? Colors.purple : Colors.black54, fontWeight: FontWeight.w700 ),),
+      leading: Icon(iconData, color:  highlighted ? Colors.purple : Colors.black54,),
+      onTap: onTap,
+    );
+  }
+}
