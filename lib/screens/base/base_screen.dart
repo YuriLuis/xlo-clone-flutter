bb87bdd9aa6abb7579d685866b1dab93fe7d58e3
yuri.pereira <yuri.pereira@ambevtech.com.br>
2021-06-18 12:34:59 -0300

    aula 346 Criando a tela de minha conta

--- lib/screens/base/base_screen.dart
+++ lib/screens/base/base_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get_it/get_it.dart';
 import 'package:mobx/mobx.dart';
+import 'package:xlo_mobx/screens/conta/conta_screen.dart';
 import 'package:xlo_mobx/screens/create/create_screen.dart';
 import 'package:xlo_mobx/screens/home/home_screen.dart';
 import 'package:xlo_mobx/stores/page_store.dart';
@@ -28,7 +29,7 @@ class _BaseScreenState extends State<BaseScreen> {
           CreateScreen(),
           Container(color: Colors.yellow),
           Container(color: Colors.purple),
-          Container(color: Colors.brown),
+          ContaScreen(),
         ],
       ),
     );
---
+++ lib/screens/conta/conta_screen.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+import 'package:get_it/get_it.dart';
+import 'package:xlo_mobx/components/custom_drawer/custom_drawer.dart';
+import 'package:xlo_mobx/stores/user_manager_store.dart';
+
+class ContaScreen extends StatelessWidget {
+  const ContaScreen({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Minha Conta'),
+        centerTitle: true,
+      ),
+      drawer: CustomDrawer(),
+      body: Center(
+        child: Card(
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
+          margin: EdgeInsets.symmetric(horizontal: 32),
+          elevation: 8,
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Container(
+                height: 140,
+                child: Stack(
+                  children: [
+                    Align(
+                      alignment: Alignment.center,
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          Text(
+                            GetIt.I<UserManagerStore>().user.name,
+                            style: TextStyle(
+                                fontSize: 20,
+                                color: Colors.purple,
+                                fontWeight: FontWeight.w900),
+                          ),
+                          Text(
+                            GetIt.I<UserManagerStore>().user.email,
+                            style:
+                                TextStyle(fontSize: 16, color: Colors.grey[700]),
+                          )
+                        ],
+                      ),
+                    ),
+                    Align(
+                      alignment: Alignment.topRight,
+                      child: FlatButton(
+                        textColor: Colors.purple,
+                        child: Text('EDITAR'),
+                        onPressed: (){},
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              Divider(color: Colors.grey[700],),
+              ListTile(
+                title: Text('Meus anúncios',
+                style: TextStyle(color: Colors.purple,
+                fontWeight: FontWeight.w600),),
+                trailing: Icon(Icons.keyboard_arrow_right),
+                onTap: (){},
+              ),
+              ListTile(
+                title: Text('Favoritos',
+                  style: TextStyle(color: Colors.purple,
+                      fontWeight: FontWeight.w600),),
+                trailing: Icon(Icons.keyboard_arrow_right),
+                onTap: (){},
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
